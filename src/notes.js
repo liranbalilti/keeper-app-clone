96af08ad50ecc5fbc6bef87ea31fae79694938cb
liran balilti <liranbalilti3@gmail.com>
2022-03-12 19:03:33 +0200

    Initial commit

---
+++ src/notes.js
@@ -0,0 +1,22 @@
+const notes = [
+    {
+        key: 1,
+        title: "Delegation",
+        content:
+            "How are you today ?"
+    },
+    {
+        key: 2,
+        title: "Loops",
+        content:
+            "Loops is "
+    },
+    {
+        key: 3,
+        title: "Arrays",
+        content:
+            "How are you today ?"
+    }
+];
+
+export default notes;
